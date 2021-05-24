39ecdae206d622e1b75529db339be8e4a42cb196
LucioHdz <luciohdz0@gmail.com>
2021-05-24 15:59:44 -0500

    Reestructurando  clase Algoritms

--- Algoritms.py
+++ Algoritms.py
@@ -1,39 +1,18 @@
 class Algoritms:
     def __init__(self):
-        self._x = None
-        self._y = None
-        self._x2 = None
-        self._y2 = None
-
-    @property
-    def cooredenadas(self):
-        """Retorna los valores x,y,x2,y2 
-
-        Returns:
-            list: Lista con los puntosx,y,x2,y2 
-        """
-        return [self._x,self._y,self._x2,self._y2]
-
-    @coordenadas.setter
-    def coordenadas(self,x,y,x2,y2):
-        """Recibe los datos de las coordenadas X y Y
+        pass
+    
+    def dda(self,x,y,x2,y2):
+        """Algoritmo DDA para graficacion
 
         Args:
             x (integer): Punto x coordenada 1
             y (integer): Punto y coordenada 1
-            x2 (integer): Punto x2 coordenada2 
-            y2 ([type]): Punto y2 coordenada2
-        """
-        self._x = x
-        self._y = y
-        self._x2 = x2
-        self._y2 = y2     
-
-    def dda(self):
-        """Genera las coordenadas a pintar  con DDA 
+            x2 (integer): Punto x coordenada 2
+            y2 (integer): Punto y coordenada 2
 
         Returns:
-            list,list: retorna las listas con datos X y Y
+            list,list: listas con los puntos a graficar
         """
         x_points = []
         y_points = []
